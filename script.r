e44add4643069a2d2e5a06fde0f9bc24359e7474
Chucaca <47813522+Chucaca@users.noreply.github.com>
2021-05-14 10:09:44 +0700

    Update script.r

--- script.r
+++ script.r
@@ -4,10 +4,22 @@ source('./r_files/flatten_HTML.r')
 libraryRequireInstall("sunburstR")
 ####################################################
 
+################### Actual code ####################
+dataset <- Values
+data1 <- data.table(dataset)
+
+data1[order(kv_session, Date), number_in_path := 1:.N, by = kv_session]
+data1[order(kv_session, Date), path := paste(location, collapse = "-"), by = kv_session]
+data1[, value := length(unique(kv_session)), by = path]
+
+path <- unique(data1[,.(path,value)])
+
+####################################################
+
 ############# Create and save widget ###############
 library(sunburstR)
 
-p = sunburst(Values,percent=FALSE);
+p = sunburst(path,percent=FALSE);
 
 internalSaveWidget(p, 'out.html');
 ####################################################
